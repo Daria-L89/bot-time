960dfe670b0014a234abc0aa705d61170cc1c581
Daria-L89 <losdashka256@gmail.com>
2026-06-22 21:57:43 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -12,6 +12,7 @@ def parse_minutes(t):
 
 def calculate(text):
     projects = {}
+    total = 0
 
     for line in text.split("\n"):
         times = re.findall(r"\d{1,2}-\d{2}", line)
@@ -21,37 +22,44 @@ def calculate(text):
             end = parse_minutes(times[1])
             duration = end - start
 
-            # Берем первое слово после времени
-            parts = line.split()
+            total += duration
 
-            if len(parts) >= 3:
-                project = parts[2]
+            line_lower = line.lower()
+
+            if "вчасно" in line_lower:
+                project = "Вчасно"
+            elif "водафон" in line_lower:
+                project = "Водафон"
             else:
                 project = "Інше"
 
             projects[project] = projects.get(project, 0) + duration
 
-    return projects
+    return projects, total
 
 async def handle(update: Update, context: ContextTypes.DEFAULT_TYPE):
     text = update.message.text
-    projects = calculate(text)
+    projects, total = calculate(text)
 
 message = "📊 Звіт\n\n"
 
 total = 0
 
 for project, minutes in projects.items():
+   message = "📊 Облік часу\n\n"
+
+for project, minutes in projects.items():
     h = minutes // 60
     m = minutes % 60
 
-    message += f"📁 {project}: {h}ч {m}хв\n"
-
-    total += minutes
+    message += f"📁 {project}: {h} год {m} хв\n"
 
 message += "\n──────────────\n"
 
-message += f"🕒 Всього: {total // 60}ч {total % 60}хв"
+h = total // 60
+m = total % 60
+
+message += f"🕒 Загалом: {h} год {m} хв"
 
 await update.message.reply_text(message)
 
